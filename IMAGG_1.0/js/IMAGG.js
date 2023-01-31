bdf1702068104431bfee4b9399558c35fc3f2e5a
manuelmsni <manuelmsni@hotmail.com>
2023-01-31 17:31:36 +0100

    Add files via upload

--- IMAGG_1.0/js/IMAGG.js
+++ IMAGG_1.0/js/IMAGG.js
@@ -4,36 +4,85 @@
  * version 1.0 || https://github.com/manuelmsni/IMAGG  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-$(document).ready(
-    function modalWindowForIMAGG(){
-        var divIMAGG = $("<div>", {id: "DivIMAGG", class: "hidden exitIMAGG"});
-        var bodyIMAGG = $("<div>", {id: "BodyIMAGG"});
-        var exitIMAGG = $("<a>", {id: "ExitIMAGG", html: "X", class: "exitIMAGG"});
-        var pExitIMAGG = $("<p>").append(exitIMAGG);
-        var figure = $("<figure>");
-        var imgIMAGG = $("<img>", {id: "IMAGG", src: ""});
-        var h2TitleIMAGG = $("<h2>", {id: "TitleIMAGG"});
-        var figcaptionIMAGG = $("<figcaption>", {id: "CaptionIMAGG"});
 
-        bodyIMAGG.append(pExitIMAGG, figure);
-        figure.append(imgIMAGG, h2TitleIMAGG, figcaptionIMAGG);
-        divIMAGG.append(bodyIMAGG);
-        $("body").append(divIMAGG);
-    }, 
-    $('.triggerIMAGG').on('click', function openModalWindow(){
-        $("body").css('overflow','hidden');
-        $("#DivIMAGG").css('overflow-y','scroll');
-        document.getElementById("IMAGG").src = $(this).attr('src');
-        $("#TitleIMAGG").text($(this).attr('title'));
-        $("#CaptionIMAGG").text($(this).attr('alt'));
-        $("#DivIMAGG").fadeIn('slow');
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
 
-        $('.exitIMAGG').on('click', function closeModalWindow(e){
-            if (e.target !== this)
-            return; 
-            $("#DivIMAGG").css('overflow','hidden');
-            $("body").css('overflow','auto');
-            $("#DivIMAGG").fadeOut('slow');
-        })
-    }) 
-);
+        /* Generates the html for the modal window */
+
+        function modalWindowForIMAGG() {
+            var divIMAGG = document.createElement("div");
+            divIMAGG.id = "DivIMAGG";
+            divIMAGG.classList.add("hidden", "exitIMAGG");
+        
+            var bodyIMAGG = document.createElement("div");
+            bodyIMAGG.id = "BodyIMAGG";
+        
+            var exitIMAGG = document.createElement("a");
+            exitIMAGG.id = "ExitIMAGG";
+            exitIMAGG.innerHTML = "X";
+            exitIMAGG.classList.add("exitIMAGG");
+        
+            var pExitIMAGG = document.createElement("p");
+            pExitIMAGG.appendChild(exitIMAGG);
+        
+            var figure = document.createElement("figure");
+        
+            var imgIMAGG = document.createElement("img");
+            imgIMAGG.id = "IMAGG";
+            imgIMAGG.src = "";
+        
+            var h2TitleIMAGG = document.createElement("h2");
+            h2TitleIMAGG.id = "TitleIMAGG";
+        
+            var figcaptionIMAGG = document.createElement("figcaption");
+            figcaptionIMAGG.id = "CaptionIMAGG";
+        
+            figure.appendChild(imgIMAGG);
+            figure.appendChild(h2TitleIMAGG);
+            figure.appendChild(figcaptionIMAGG);
+        
+            bodyIMAGG.appendChild(pExitIMAGG);
+            bodyIMAGG.appendChild(figure);
+        
+            divIMAGG.appendChild(bodyIMAGG);
+            document.body.appendChild(divIMAGG);
+        };
+
+        modalWindowForIMAGG();
+
+        /* Triggers to open the modal window */
+
+        var openTriggers = document.querySelectorAll(".triggerIMAGG");
+
+        openTriggers.forEach(function (trigger) {
+
+            trigger.addEventListener("click", function triggerIMGG() {
+                document.body.style.overflow = "hidden";
+                document.getElementById("DivIMAGG").style.overflowY = "scroll";
+                document.getElementById("IMAGG").src = this.getAttribute("src");
+                document.getElementById("TitleIMAGG").innerText = this.getAttribute("title");
+                document.getElementById("CaptionIMAGG").innerText = this.getAttribute("alt");
+                document.getElementById("DivIMAGG").style.display = "block";
+            });
+        
+            /* Triggers to close the modal window */ 
+        
+            var closeTriggers = document.querySelectorAll(".exitIMAGG");
+
+            closeTriggers.forEach(function (trigger) {
+            
+                trigger.addEventListener("click", function (e) {
+                    if (e.target !== this) {
+                    return;
+                    }
+                    document.getElementById("DivIMAGG").style.overflow = "hidden";
+                    document.body.style.overflow = "auto";
+                    document.getElementById("DivIMAGG").style.display = "none";
+                });
+             
+            });
+            
+        });
+    }
+};
